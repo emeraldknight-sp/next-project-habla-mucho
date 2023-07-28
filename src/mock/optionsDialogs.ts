80cd96d09e8b19f294926f80ecc0865c9f1b5d44
David <contato.davidalmeida@outlook.com>
2023-07-28 15:07:35 -0300

    🤡 Mock things

---
+++ src/mock/optionsDialogs.ts
@@ -0,0 +1,42 @@
+export const optionsDialogs = [
+	{
+		title: "Random",
+		icon: "/surprise.png",
+		color: "from-green to-blue",
+	},
+	{
+		title: "Travel",
+		icon: "/travel.png",
+		color: "from-purple to-blue",
+	},
+	{
+		title: "Survival",
+		icon: "/survival.png",
+		color: "from-pink to-purple",
+	},
+	{
+		color: "from-yellow to-red",
+		title: "Business",
+		icon: "/handshake.png",
+	},
+	{
+		title: "Customer service",
+		icon: "/conversation.png",
+		color: "from-blue to-lightBlue",
+	},
+	{
+		title: "Culture",
+		icon: "/drum.png",
+		color: "from-blue to-green",
+	},
+	{
+		title: "Funny",
+		icon: "/fun.png",
+		color: "from-red to-yellow",
+	},
+	{
+		title: "Educational",
+		icon: "/book.png",
+		color: "from-red to-pink",
+	},
+];
