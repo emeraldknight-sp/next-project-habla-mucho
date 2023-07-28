37ace8a238b072cbaf4f3c65545271fd73753093
David <contato.davidalmeida@outlook.com>
2023-07-28 11:30:12 -0300

    🤡 Mock things

---
+++ src/mock/optionsAmout.ts
@@ -0,0 +1,1 @@
+export const optionsAmount: string[] = ["2", "3", "4", "5"];
---
+++ src/mock/optionsLanguage.ts
@@ -0,0 +1,24 @@
+import { optionLanguage } from "@/interfaces/optionLanguage";
+
+export const optionsLanguage: optionLanguage[] = [
+	{
+		icon: "/brazil.png",
+		language: "Português",
+		id: "PT",
+	},
+	{
+		icon: "/united-states-america.png",
+		language: "English",
+		id: "EN",
+	},
+	{
+		icon: "/spain.png",
+		language: "Español",
+		id: "ES",
+	},
+	{
+		icon: "/italia.png",
+		language: "Italiano",
+		id: "IT",
+	},
+];
