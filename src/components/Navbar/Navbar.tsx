d0611d3ba09cb4aa1557d97fa6ac98c8f0a89c17
David <contato.davidalmeida@outlook.com>
2023-07-28 14:17:08 -0300

    ✅ Update: Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,32 @@
-import Link from "next/link";
 import { FiHome, FiList, FiSettings } from "react-icons/fi";
 
-export const Navbar = () => {
+import { NavbarProps } from "@/interfaces/NavbarProps";
+
+export const Navbar = ({ handleClick }: NavbarProps) => {
 	return (
 		<nav className="fixed bottom-0 left-0 w-full bg-yellow px-4 py-2 lg:hidden">
 			<ul className="flex flex-row justify-center items-center gap-8 ">
-				<li className="bg-white rounded-full shadow-lg p-2">
-					<Link href="/language" className="text-yellow">
-						<FiList size={24} />
-					</Link>
-				</li>
-				<li className="bg-white rounded-full shadow-lg p-2">
-					<Link href="/" className="text-yellow">
-						<FiHome size={24} />
-					</Link>
-				</li>
-				<li className="bg-white rounded-full shadow-lg p-2">
-					<Link href="/settings" className="text-yellow">
-						<FiSettings size={24} />
-					</Link>
-				</li>
+				<button
+					type="button"
+					className="bg-white text-yellow p-2 rounded-full"
+					onClick={() => handleClick("left")}
+				>
+					<FiList size={24} />
+				</button>
+				<button
+					type="button"
+					className="bg-white text-yellow rounded-full shadow-lg p-2"
+					onClick={() => handleClick("")}
+				>
+					<FiHome size={24} />
+				</button>
+				<button
+					type="button"
+					className="bg-white text-yellow rounded-full shadow-lg p-2"
+					onClick={() => handleClick("right")}
+				>
+					<FiSettings size={24} />
+				</button>
 			</ul>
 		</nav>
 	);
