2234531a4ed57829803d74a89068995f4a11265f
David <contato.davidalmeida@outlook.com>
2023-09-09 11:24:34 -0300

    🤡 Mock things

---
+++ src/mock/usersData.ts
@@ -0,0 +1,46 @@
+import { UserProps } from "@/interfaces/UserProps";
+
+export const usersData: UserProps[] = [
+	{
+		id: 1,
+		firstName: "Walter",
+		lastName: "White",
+		avatar: "/heisenberg.webp",
+		occupation: "Professor",
+	},
+	{
+		id: 2,
+		firstName: "Jesse",
+		lastName: "Pinkman",
+		avatar: "/jesse.webp",
+		occupation: "Estudante",
+	},
+	{
+		id: 3,
+		firstName: "Jane",
+		lastName: "Margolis",
+		avatar: "/jane.webp",
+		occupation: "",
+	},
+	{
+		id: 4,
+		firstName: "Saul",
+		lastName: "Goodman",
+		avatar: "/jimmy.webp",
+		occupation: "Advogado",
+	},
+	{
+		id: 5,
+		firstName: "Mike",
+		lastName: "Ehrmantraut",
+		avatar: "/ehrmantraut.webp",
+		occupation: "Investigador",
+	},
+	{
+		id: 6,
+		firstName: "Gustavo",
+		lastName: "Fring",
+		avatar: "/gus.webp",
+		occupation: "Empresário",
+	},
+];
