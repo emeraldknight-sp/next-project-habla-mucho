d7cd4976f19d4a3ffad14c880ea2aa8be7f3b7c3
David <contato.davidalmeida@outlook.com>
2023-09-09 11:23:43 -0300

    🔥 Remove code or files

--- src/app/dialog/page.tsx
+++ src/app/dialog/page.tsx
@@ -1,64 +1,26 @@
 "use client";
 
 import Image from "next/image";
-
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import { DocumentData } from "@firebase/firestore-types";
-import { getChats } from "@/api";
 
-import { SidebarLeft, SidebarRight } from "@/components/Sidebars";
-import { Navbar } from "@/components/Navbar";
 import { Main } from "@/components/Main";
+import { Navbar } from "@/components/Navbar";
+import { SidebarLeft, SidebarRight } from "@/components/Sidebars";
+
+import { ChatContext } from "@/context/ChatContext";
+import { UsersContext } from "@/context/UsersContext";
 
 import { usersData } from "@/mock/usersData";
-import { optionsData } from "@/mock/optionsData";
-
-import { AppContext } from "@/context/AppContext";
-import { UsersContext } from "@/context/UsersContext";
-import { useSearchParams } from "next/navigation";
 
 export default function Dialog() {
-	const { language, peopleCount } = useContext(AppContext);
-	const [data, setData] = useState<DocumentData[]>([]);
-	const [loading, setLoading] = useState<boolean>(true);
-
-	const searchParams = useSearchParams();
-	const search = searchParams.get("d");
-
+	const { chat, loading } = useContext(ChatContext);
 	const { firstName } = useContext(UsersContext);
 
 	const users = usersData.filter((element) => element.firstName !== firstName);
 	const userIndex = Math.floor(Math.random() * users.length);
 	const user = users[userIndex];
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const chats = await getChats();
-				localStorage.setItem("data", JSON.stringify(chats));
-				setData(chats);
-			} catch (error) {
-				console.error("Erro ao obter os chats:", error);
-				setData(optionsData);
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		fetchData();
-	}, []);
-
-	console.log("DATA LOG", data.length)
-
-	const chats = data
-		.filter((element) => element.category === search || "random" === search)
-		.filter(
-			(element) =>
-				element.language === language && element.people === peopleCount,
-		);
-	const chatIndex = Math.floor(Math.random() * chats.length);
-	const chat = chats[chatIndex];
 
 	if (loading) {
 		return <div className="text-black">Carregando...</div>;
