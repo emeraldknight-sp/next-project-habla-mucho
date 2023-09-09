7b938d507edce21530d7f950643ecb7377c126a1
David <contato.davidalmeida@outlook.com>
2023-09-09 11:23:12 -0300

    ✅ Update: UsersContext context

--- src/context/UsersContext.tsx
+++ src/context/UsersContext.tsx
@@ -4,71 +4,14 @@ import { createContext, useEffect, useMemo, useState } from "react";
 
 import { ComponentProps } from "@/interfaces/ComponentProps";
 import { UserProps } from "@/interfaces/UserProps";
+import { usersData } from "@/mock/usersData";
 
-export const UsersContext = createContext<UserProps>({
-	id: 0,
-	firstName: "",
-	lastName: "",
-	avatar: "",
-	occupation: "",
-});
+export const UsersContext = createContext<UserProps>(usersData[1]);
 
 export const UsersProvider = ({ children }: ComponentProps) => {
-	const [user, setUser] = useState<UserProps>({
-		id: 0,
-		firstName: "",
-		lastName: "",
-		avatar: "",
-		occupation: "",
-	});
+	const [user, setUser] = useState<UserProps>(usersData[1]);
 
-	const users = useMemo(
-		() => [
-			{
-				id: 1,
-				firstName: "Walter",
-				lastName: "White",
-				avatar: "/heisenberg.webp",
-				occupation: "Professor",
-			},
-			{
-				id: 2,
-				firstName: "Jesse",
-				lastName: "Pinkman",
-				avatar: "/jesse.webp",
-				occupation: "Estudante",
-			},
-			{
-				id: 3,
-				firstName: "Jane",
-				lastName: "Margolis",
-				avatar: "/jane.webp",
-				occupation: "",
-			},
-			{
-				id: 4,
-				firstName: "Saul",
-				lastName: "Goodman",
-				avatar: "/jimmy.webp",
-				occupation: "Advogado",
-			},
-			{
-				id: 5,
-				firstName: "Mike",
-				lastName: "Ehrmantraut",
-				avatar: "/ehrmantraut.webp",
-				occupation: "Investigador",
-			},
-			{
-				id: 6,
-				firstName: "Gustavo",
-				lastName: "Fring",
-				avatar: "/gus.webp",
-				occupation: "Empresário",
-			},
-		],
-		[],
-	);
+	const users = useMemo(() => usersData, []);
 
 	useEffect(() => {
 		const index = Math.floor(Math.random() * users.length);
