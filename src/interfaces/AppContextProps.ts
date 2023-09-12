a2713ea54e5829bde1764677207d4b808fd6636b
David <contato.davidalmeida@outlook.com>
2023-09-12 15:39:24 -0300

    ✅ Update: AppContext context and interface

--- src/interfaces/AppContextProps.ts
+++ src/interfaces/AppContextProps.ts
@@ -1,6 +1,8 @@
 export interface AppContextProps {
 	language: string;
 	peopleCount: number;
+	theme: boolean;
 	setLanguage: (selectedLanguage: string) => void;
 	setPeopleCount: (selectedPeopleCount: number) => void;
+	setTheme: (selectedTheme: boolean) => void;
 }
